79e1e24865fa728a79d5d4d35628534fc00abdc8
rafaelfelixs <34246480+rafaelfelixs@users.noreply.github.com>
2019-04-09 11:02:32 -0300

    Create Mensagem.java

---
+++ Mensagem.java
@@ -0,0 +1,26 @@
+
+public class Mensagem {
+
+	private int id;
+	private Pessoa pessoa;
+	private String corpo;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	public String getCorpo() {
+		return corpo;
+	}
+	public void setCorpo(String corpo) {
+		this.corpo = corpo;
+	}
+}
